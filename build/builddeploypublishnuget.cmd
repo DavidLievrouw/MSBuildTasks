614dc8f075c175fd832206317ac40c0993e22009
davidlievrouw <david.lievrouw@gmail.com>
2015-08-26 21:45:09 +0200

    Add NuGet publishing support

--- build/builddeploypublishnuget.cmd
+++
@@ -1,6 +0,0 @@
-cls
-echo off
-SET DIR=%~dp0%
-IF NOT EXIST "%DIR%log" MKDIR "%DIR%log"
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /m /v:n "%DIR%msbuildtasks.proj" /target:BuildDeployPublishNuGet /logger:FileLogger,Microsoft.Build.Engine;LogFile=%DIR%log/builddeploypublishnuget.log
-pause
